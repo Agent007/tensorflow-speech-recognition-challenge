206f896106945541e62ccbaace2ebb4c19438b4d
J <jeffreyl@alumni.cmu.edu>
2017-11-23 23:05:20 -0800

    slightly improved score to 0.79 from 0.78 by increasing background volume to 0.5 from default of 0.1, batch size to 512 from default of 100, and number of fine tuning training steps by an additional 3000

--- train.sh
+++ train.sh
@@ -1,4 +1,9 @@
 #!/bin/bash -e
 
 source activate
-python ~/tensorflow/tensorflow/examples/speech_commands/train.py --data_url '' --data_dir data/train/audio --testing_percentage 0 --validation_percentage 1 --summaries_dir data/retrain_logs --train_dir data/speech_commands_train
+#TRAINING_SIZE=64073  # math.floor(64721 * .99)
+BATCH_SIZE=512
+EVAL_STEP_INTERVAL=128  # approximately TRAINING_SIZE / BATCH_SIZE
+python ~/tensorflow/tensorflow/examples/speech_commands/train.py --data_url '' --data_dir data/train/audio --background_volume 0.5 --testing_percentage 0 --validation_percentage 1 --how_many_training_steps 15000,3000 --learning_rate 0.001,0.0001 --batch_size ${BATCH_SIZE} --summaries_dir data/retrain_logs --train_dir data/speech_commands_train
+
+python ~/tensorflow/tensorflow/examples/speech_commands/train.py --data_url '' --data_dir data/train/audio --background_volume 0.5 --testing_percentage 0 --validation_percentage 1 --how_many_training_steps 21000 --learning_rate 0.0001 --batch_size ${BATCH_SIZE} --summaries_dir data/retrain_logs --train_dir data/speech_commands_train --start_checkpoint data/speech_commands_train/conv.ckpt-18000
